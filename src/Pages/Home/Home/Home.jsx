babadf76a6e30d34c4104b4990c414fe39babdd2
reshad21 <miakala7@gmail.com>
2022-12-14 00:05:08 +0600

    react-revel package install successfully

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 import AboutMe from '../AboutMe/AboutMe';
 import Banner from '../BannerSection/Banner';
 import Gallery from './../Gallery/Gallery';
-
 const Home = () => {
     return (
         <div className='max-w-[1200px] mx-auto'>
-            <Banner></Banner>
-            <Gallery></Gallery>
-            <AboutMe></AboutMe>
+            <Fade bottom>
+                <Banner></Banner>
+            </Fade>
+            <Fade bottom>
+                <Gallery></Gallery>
+            </Fade>
+            <Fade bottom>
+                <AboutMe></AboutMe>
+            </Fade>
         </div>
     );
 };
