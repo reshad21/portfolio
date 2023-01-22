3da76e225968a282eea0bedad6cb7f1dc0d25229
reshad21 <miakala7@gmail.com>
2023-01-22 23:35:06 +0600

    usestate concept

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Blog = () => {
+    const [toggle, setToggle] = useState('Upcoming blog');
+    const handleToggle = () => {
+        let value = toggle;
+        (value === 'Upcoming blog') ? setToggle('Upcoming blog') : setToggle('Coming Soon...!');
+        // if (value === 'Upcoming blog') {
+        //     setToggle('Coming Soon...!');
+        // } else {
+        //     setToggle('Upcoming blog');
+        // }
+        console.log(toggle);
+    }
     return (
-        <div className='flex justify-center items-center h-screen'>
-            <h1 className='text-4xl font-bold text-rose-700'>Coming Soon...!</h1>
+        <div className='flex flex-col justify-center items-center h-screen'>
+            <h1 className='text-4xl font-bold text-rose-700'>{toggle}</h1>
+            <button onClick={handleToggle} className='btn btn-outline btn-secondary'>Change</button>
         </div>
     );
 };
